319c325a4e8ef8079dbea4a6f943a72e4a9270dc
Helal Ferrari Cabral <ferrari.work@gmail.com>
2026-01-27 10:15:10 -0300

    retirando acoplamentos desnecessários e caminhado para objeto de dominio para emissao dos relatorios

--- src/BankStatementProcessor.java
+++ src/BankStatementProcessor.java
@@ -2,11 +2,40 @@ import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BankTransactionProcessor {
+public class BankStatementProcessor {
     private final List<BankTransaction> bankTransactions;
 
-    public BankTransactionProcessor(final List<BankTransaction> bankTransactions) {
+    public BankStatementProcessor(final List<BankTransaction> bankTransactions) {
         this.bankTransactions = bankTransactions;
+    }
+
+    public double summarizeTransactions(final BankTransactionSummarizer bankTransactionSummarizer) {
+
+        double result = 0;
+        for(final BankTransaction bankTransaction : bankTransactions) {
+            result = bankTransactionSummarizer.summarize(result, bankTransaction);
+        }
+        return result;
+    }
+
+    public double calculateTotalInMonth(final Month month) {
+        return summarizeTransactions((acc, bankTransactions) ->
+                bankTransactions.getDate().getMonth() == month ? acc +
+                        bankTransactions.getAmount() : acc);
+    }
+
+    public List<BankTransaction> findTransactions(final BankTransactionFilter bankTransactionFilter) {
+        final List<BankTransaction> result = new ArrayList<>();
+        for(final BankTransaction bankTransaction : bankTransactions) {
+            if(bankTransactionFilter.test(bankTransaction)) {
+                result.add(bankTransaction);
+            }
+        }
+        return result;
+    }
+
+    public List<BankTransaction> findTransactionsGreaterThanEqual(final int amount) {
+        return findTransactions(bankTransaction -> bankTransaction.getAmount() >= amount);
     }
 
     public double calculateTotalAmount() {
@@ -28,13 +57,7 @@ public class BankTransactionProcessor {
     }
 
     public List<BankTransaction> findTransactionGreaterThanEqual(final int amount) {
-        final List<BankTransaction> result = new ArrayList<>();
-        for(final BankTransaction bankTransaction : bankTransactions) {
-            if(bankTransaction.getAmount() >= amount) {
-                result.add(bankTransaction);
-            }
-        }
-        return result;
+        return findTransactions(bankTransaction -> bankTransaction.getAmount() >= amount);
     }
 
     public List<BankTransaction> findTransactionInMonth(final Month month) {
@@ -52,16 +75,6 @@ public class BankTransactionProcessor {
         for(final BankTransaction bankTransaction : bankTransactions) {
             if(bankTransaction.getDate().getMonth() == month &&
             bankTransaction.getAmount() >= amount) {
-                result.add(bankTransaction);
-            }
-        }
-        return result;
-    }
-
-    public List<BankTransaction> findTransactions(final BankTransactionFilter bankTransactionFilter) {
-        final List<BankTransaction> result = new ArrayList<>();
-        for(final BankTransaction bankTransaction : bankTransactions) {
-            if(bankTransactionFilter.test(bankTransaction)) {
                 result.add(bankTransaction);
             }
         }
